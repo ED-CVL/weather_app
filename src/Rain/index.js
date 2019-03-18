fdc01a0f92540e4d7bad004cfe0e0d2b0d912ff0
Eric Dirr <edirr92@aol.com>
2019-03-18 14:46:41 -0400

    re-rendering video images

--- src/Rain/index.js
+++ src/Rain/index.js
@@ -1,45 +1,43 @@
 import React, { Component } from "react";
 import './style.css';
-import anime from 'animejs';
 import { CSSTransitionGroup } from 'react-transition-group';
-
-function rainDrop(key, startPostion, randomDelay) {
-  const styles = {
-    left: startPostion+'%',
-    animationDelay: randomDelay+'s',
-  }
-  return <div className={'rain-drop'} key={'rain-drop'+key} style={styles}/>
-}
-
-function cloud(number) {
-  return <div className={'cloud cloud' + number} />
-}
 
 class Rain extends Component {
   constructor(props){
     super(props);
     this.state = {
     }
+    this.renderVideo = this.renderVideo.bind(this);
   }
 
-  renderVideo(weatherDesc) {
-    switch(weatherDesc){
-      case "clouds":
-      return "https://s3.amazonaws.com/project-4-bucket/Pexels+Videos+3611.mp4";
-      case "snow":
-      return "https://s3.amazonaws.com/project-4-bucket/Falling+Snowflakes.mp4";
-      case "thunderstorm":
-      return "https://s3.amazonaws.com/project-4-bucket/lightning02.mp4";
-      case "rain":
-      return "https://s3.amazonaws.com/project-4-bucket/rain.mp4";
-      case "sun":
-      return "https://s3.amazonaws.com/project-4-bucket/Footage+Of+Sunlight.mp4";
-    }
-  }
 
+renderVideo(weather){
+  console.log(weather);
+  switch(true) {
+    // case (desc.includes("overcast")|| desc.includes("scattered")):
+    //   return <FontAwesomeIcon className="overcast-icon" icon="cloud-sun"/>;
+    case (weather.includes("clear")):
+      return <video key="1" autoPlay muted loop className="video">
+      <source src="https://s3.amazonaws.com/project-4-bucket/Footage+Of+Sunlight.mp4" type="video/mp4"/></video>;
+    case (weather.includes("rain")|| weather.includes("drizzle")||weather.includes("mist")):
+      return <video key="2" autoPlay muted loop className="video">
+      <source src="https://s3.amazonaws.com/project-4-bucket/rain.mp4" type="video/mp4"/></video>;
+    case (weather.includes("snow")):
+      return <video key="3" autoPlay muted loop className="video">
+      <source src="https://s3.amazonaws.com/project-4-bucket/Falling+Snowflakes.mp4" type="video/mp4"/></video>;
+    case (weather.includes("cloud")):
+      return <video key="4" autoPlay muted loop className="video">
+      <source src="https://s3.amazonaws.com/project-4-bucket/Pexels+Videos+3611.mp4" type="video/mp4"/></video>;
+    case (weather.includes("thunder")):
+      return <video key="5" autoPlay muted loop className="video">
+      <source src="https://s3.amazonaws.com/project-4-bucket/lightning02.mp4" type="video/mp4"/></video>;
+    default:
+      return null;
+  }}
   render() {
+    const weather = this.props.weather < 1? "" : this.props.weather.weather_description;
     return(
-      <div id='rain'>
+      this.props.weather < 1? "" : <div id='rain'>
 
       <CSSTransitionGroup
       transitionName="example"
@@ -47,9 +45,7 @@ class Rain extends Component {
       transitionAppearTimeout={500}
       transitionEnter={false}
       transitionLeave={false}>
-        <video autoPlay muted loop className="video">
-          <source src={this.renderVideo("clouds")} type="video/mp4"/>
-        </video>
+      {this.renderVideo(weather)}
       </CSSTransitionGroup>
       </div>
     )
