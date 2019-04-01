1861aa7cdd3bef0d456af35152a14dcf28a00e12
Eric Dirr <edirr92@aol.com>
2019-04-01 12:22:33 -0400

    removed bulma logo

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -17,8 +17,8 @@ class Nav extends Component {
     return (
         <nav className="navbar is-link" role="navigation" aria-label="main navigation">
   <div className="navbar-brand">
-    <a className="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+    <a className="navbar-item" href="">
+     
     </a>
 
     <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
